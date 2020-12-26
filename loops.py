5ccdeb0388e5c456148d8ea54fdc9a370f8391bc
MOHIT KUMAR <73468581+mohitkumarfifth@users.noreply.github.com>
2020-12-26 23:55:31 +0530

    files uploaded

---
+++ loops.py
@@ -0,0 +1,8 @@
+for i in[0,1,2,3,4,5]:
+    print(i)
+names =["Harry","Ron","Hermoine"]   
+for name in names:
+    print(name) 
+name="Harry"
+for character in name:
+    print(character)
---
+++ sequences.py
@@ -0,0 +1,6 @@
+name=["Harry","Ron","Hermione"]
+print(name[0])
+print(name)
+coordinateX=10.0
+coordinateY=20.0
+coordinate=(10.0,20.0) 
